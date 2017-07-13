7d6b130f547f88933b83df69cd410df663363747
Stephan Seebacher <stephan.seebacher@gmail.com>
2017-07-13 18:07:22 +0200

    logger nullhandler (to reduce no handler found warnings)

--- phenograph/__init__.py
+++ phenograph/__init__.py
@@ -1,2 +1,6 @@
 from phenograph.cluster import cluster
 from phenograph.classify import classify
+
+import logging
+from logging import NullHandler
+logging.getLogger(__name__).addHandler(NullHandler())
